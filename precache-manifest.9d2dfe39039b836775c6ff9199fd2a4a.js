21d465ebcf2d74528ec6bd4c9293600adef25c89
Szymon Janiak <szymonjaniak5@gmail.com>
2020-07-26 07:51:59 +0200

    Updates

---
+++ precache-manifest.9d2dfe39039b836775c6ff9199fd2a4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee5563ddb7243be121402e72c4ad885",
+    "url": "/BMI_Calculator/index.html"
+  },
+  {
+    "revision": "85ebe597ef524789c373",
+    "url": "/BMI_Calculator/static/css/main.d1550f22.chunk.css"
+  },
+  {
+    "revision": "8c3f1d7c1df32f990464",
+    "url": "/BMI_Calculator/static/js/2.5bf19f29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BMI_Calculator/static/js/2.5bf19f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ebe597ef524789c373",
+    "url": "/BMI_Calculator/static/js/main.10634f9e.chunk.js"
+  },
+  {
+    "revision": "2051bb16a38634c1cac4",
+    "url": "/BMI_Calculator/static/js/runtime-main.b096c846.js"
+  },
+  {
+    "revision": "b7d2b1d7cbbc11ce31578121f52363bb",
+    "url": "/BMI_Calculator/static/media/pablo-healthy-life.b7d2b1d7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1a3cf8da5edbae1c46690c13febf55c.js"
+  "/BMI_Calculator/precache-manifest.9d2dfe39039b836775c6ff9199fd2a4a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BMI_Calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
