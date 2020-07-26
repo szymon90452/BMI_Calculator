e56f189bb213efaa045c1e4ef87c24d4c2172197
Szymon Janiak <szymonjaniak5@gmail.com>
2020-07-26 07:46:29 +0200

    Updates

---
+++ precache-manifest.e1a3cf8da5edbae1c46690c13febf55c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eaa456231d419753838cae2d7d48943",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd011f0dbd2e057acb3c",
+    "url": "/static/css/main.4aa0580c.chunk.css"
+  },
+  {
+    "revision": "8c3f1d7c1df32f990464",
+    "url": "/static/js/2.5bf19f29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5bf19f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd011f0dbd2e057acb3c",
+    "url": "/static/js/main.fd577c3c.chunk.js"
+  },
+  {
+    "revision": "0fc79dfd5cd40bb36310",
+    "url": "/static/js/runtime-main.b38e052a.js"
+  },
+  {
+    "revision": "b7d2b1d7cbbc11ce31578121f52363bb",
+    "url": "/static/media/pablo-healthy-life.b7d2b1d7.png"
+  }
+]);
